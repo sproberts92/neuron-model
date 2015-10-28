7ba43fe4bb693883c023cc96f6769975bb7ab5ee
Samuel Roberts <sproberts92@outlook.com>
2015-10-28 23:56:43 +0100

    Compartmentalise into brain class

--- src/neural-growth.cpp
+++ src/neural-growth.cpp
@@ -5,43 +5,68 @@ using namespace std;
 #include <fstream>
 #include <sstream>
 
-#include "neuron.h"
+#include "brain.h"
+
+void print_axon(int dim, node *base, std::ofstream &out_stream);
 
 int main()
 {
 	clock_t begin = clock();
 
 	int dim = 3;
-	int n_neurons = 5;
-	int growth_iter = 200;
+	int n_neurons = 15;
+	int growth_iter = 100;
+	double schwann_l = 1.0;
 	std::vector<double> bounds = {-50,50,-50,50,-50,50};
-	
-	// Not currently used, future: used to create variation in Schwann cell length
-	// std::vector<double> s_bounds = {7.7, 7.8};
-	
+		
 	neuron *neurons = new neuron[n_neurons];
 	
+	brain brain_1(dim, bounds, n_neurons, schwann_l);
+	brain_1.place_neurons();
 
-	for (int i = 0; i < n_neurons; i++)
-		neurons[i] = neuron(dim, bounds);
-		
-	for (int it = 0; it <= growth_iter; it++)
+	for (int it = 0; it < growth_iter; it++)
 	{
 		std::ostringstream fileName;
 		fileName << "output\\output_"<< it << ".dat";
-		
-		for (int i = 0; i < n_neurons; i++)
-				neurons[i].grow_axon(1.0, bounds);
-		
+
+		brain_1.grow_axons();
+
 		ofstream outfile;
-		outfile.open(fileName.str());
+		outfile.open(fileName.str(), 'w');
 		for (int i = 0; i < n_neurons; i++)
-			neurons[i].print_axon(outfile);
+			print_axon(dim, brain_1.neurons[i].base_soma, outfile);
 		outfile.close();
-	}	
+	}
+
+	brain_1.connect_network();
+
+	ofstream outfile;
+	outfile.open("output\\finalprint.dat", std::ofstream::trunc);
+		for (int i = 0; i < n_neurons; i++)
+			print_axon(dim, brain_1.neurons[i].base_soma, outfile);
+	outfile.close();
 
 	clock_t end = clock();
 	std::cout << double(end - begin) /CLOCKS_PER_SEC << std::endl;
 
 	return 0;
+}
+
+void print_axon(int dim, node *base, std::ofstream &out_stream)
+{
+	node *list_ptr;
+	list_ptr = base;
+
+	if(list_ptr != 0)
+		while(!list_ptr->next.empty())
+		{
+			for (int i = 0; i < dim; i++)
+				out_stream << list_ptr->pos[i] << " ";
+			out_stream << std::endl;
+
+			for (int i = 1; i < list_ptr->next.size(); i++)
+				print_axon(dim, list_ptr->next[i], out_stream);
+
+			list_ptr = list_ptr->next[0];
+		}
 }
