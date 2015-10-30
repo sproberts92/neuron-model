2b64dc693879db4153603e58aca8a4b8c0ea4724
Samuel Roberts <sproberts92@outlook.com>
2015-10-30 16:43:22 +0100

    Parallelised using multiprocessing module

--- visualisation/Visualisation.py
+++ visualisation/Visualisation.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from multiprocessing import Pool
 
 def read_points(path):
     f = open(path, 'r')
@@ -15,36 +16,39 @@ def read_points(path):
 
     return points
 
-def main():
-    limits = 50
+def f(i, fig, ax):
+    sigpath = "output\\signal\\signal_" + str(i) + ".dat"
 
+    points = read_points(sigpath)
+
+    lines = ax.scatter(*zip(*points), c='r')
+    ax.view_init(30, 0.3 * i)
+    fig.canvas.draw()
+
+    plt.savefig("output\\frames\\frame" + str(i) + ".png", format='png')
+    lines.remove()
+
+    print("Finished processing frame {0}".format(i))
+
+def main():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection = '3d')
 
+    netpath = "output\\Complete_Network.dat"
+    points = read_points(netpath)
+    net = ax.scatter(*zip(*points), c='b', marker='.', s=5, alpha=0.1)
+
+    limits = 50
     ax.set_xlim3d(-limits,limits)
     ax.set_ylim3d(-limits,limits)
     ax.set_zlim3d(-limits,limits)
 
     ax.view_init(30,0)
 
-    netpath = "output\\Complete_Network.dat"
-    points = read_points(netpath)
-    net = ax.scatter(*zip(*points), c='b', marker='.', s=5, alpha=0.1)
-
-    lines = []
-    for i in range(500):
-        sigpath = "output\\signal\\signal_" + str(i) + ".dat"
-
-        points = read_points(sigpath)
-
-        lines = ax.scatter(*zip(*points), c='r')
-        ax.view_init(30, 0.3 * i)
-        fig.canvas.draw()
-
-        plt.savefig("output\\frames\\frame" + str(i) + ".png", format='png')
-        lines.remove()
-
-        print("Finished processing frame {0}".format(i))
+    args = [(i, fig, ax) for i in range(500)]
+    
+    p = Pool(4)
+    p.starmap(f, args, 16)
 
 if __name__ == '__main__':
     main()
