aadf69b0757fa6a7a5c9c1899d5ec4c52f86db2f
Samuel Roberts <sproberts92@outlook.com>
2015-11-10 17:39:41 +0100

    Move printing of signal propagation to the end

--- src/neural-growth.cpp
+++ src/neural-growth.cpp
@@ -138,20 +138,30 @@ int main()
 			std::cout << "Synapse error!" << std::endl;
 	}
 
-	/* Reset newtwork */
+	/*********End naive pruning algorithm ************/
+
+	// Reset newtwork
 	brain.clear_signals();
 	brain.neurons[0].base_soma->value = 1;
-	/*********End naive pruning algorithm ************/
+
+	std::cout << "Writing signal propagation frames..." << std::endl;
 
 	for (int i = 0; i < config.prop_iter; i++)
 	{
+		std::cout << 100 * i / config.growth_iter << "%\r";
+
 		brain.propagate_signal(0.0);
 
 		std::ostringstream fileName;
 		fileName << "output\\signal\\signal_" << i << ".dat";
-		brain.print_network(fileName, 0);	
+		brain.print_network(fileName, 0);
+
+		// Kill the signal
+		if(brain.neurons[0].base_soma->value == 1)
+			brain.neurons[0].base_soma->value = 0;
 	}
 
+	std::cout << "100%\n" << std::endl;
 
 	std::cout << "Network size: " << brain.all_nodes.size() << " nodes." << std::endl;
 	std::cout << "Connections: " << brain.all_synapses.size() << " synapses." << std::endl << std::endl;
