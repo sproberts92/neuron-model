afe127c52bf27af33ee820049b1ffce889ee78bf
Samuel Roberts <sproberts92@outlook.com>
2015-11-26 15:33:54 +0100

    Rewrite with ternary operators
    Easier handling of false case, which was not included before.

--- src/interface_win.cpp
+++ src/interface_win.cpp
@@ -2,11 +2,11 @@
 
 void read_key_state(struct key_state_t &key_state)
 {
-	if(GetKeyState(0x1B) & 0x8000) key_state.esc = true;
+	key_state.esc = (GetKeyState(0x1B) & 0x8000) ? true : false;
 	
-	if(GetKeyState(0x41) & 0x8000) key_state.a = true;
-	if(GetKeyState(0x4C) & 0x8000) key_state.l = true;
+	key_state.a = (GetKeyState(0x41) & 0x8000) ? true : false;
+	key_state.l = (GetKeyState(0x4C) & 0x8000) ? true : false;
 
-	if(GetKeyState(0x47) & 0x8000) key_state.g = true;
-	if(GetKeyState(0x42) & 0x8000) key_state.b = true;
+	key_state.g = (GetKeyState(0x47) & 0x8000) ? true : false;
+	key_state.b = (GetKeyState(0x42) & 0x8000) ? true : false;
 }
