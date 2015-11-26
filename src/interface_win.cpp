8f81c185429ba483ec648de09df0705794f1eeb2
Samuel Roberts <sproberts92@outlook.com>
2015-11-26 19:02:57 +0100

    Change hex codes to constants

--- src/interface_win.cpp
+++ src/interface_win.cpp
@@ -2,11 +2,11 @@
 
 void read_key_state(struct key_state_t &key_state)
 {
-	key_state.esc = (GetKeyState(0x1B) & 0x8000) ? true : false;
+	key_state.esc = (GetKeyState(0x1B) & WM_KEYDOWN) ? true : false;
 	
-	key_state.a = (GetKeyState(0x41) & 0x8000) ? true : false;
-	key_state.l = (GetKeyState(0x4C) & 0x8000) ? true : false;
+	key_state.a = (GetKeyState(0x41) & WM_KEYDOWN) ? true : false;
+	key_state.l = (GetKeyState(0x4C) & WM_KEYDOWN) ? true : false;
 
-	key_state.g = (GetKeyState(0x47) & 0x8000) ? true : false;
-	key_state.b = (GetKeyState(0x42) & 0x8000) ? true : false;
+	key_state.g = (GetKeyState(0x47) & WM_KEYDOWN) ? true : false;
+	key_state.b = (GetKeyState(0x42) & WM_KEYDOWN) ? true : false;
 }
