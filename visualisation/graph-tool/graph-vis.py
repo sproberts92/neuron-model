9548077bfbaaf09c4afebf8896858f2c8f9c67eb
Samuel Roberts <sproberts92@outlook.com>
2015-11-27 15:06:56 +0100

    Add graph-vis.py for graph-tool visualisation

---
+++ visualisation/graph-tool/graph-vis.py
@@ -0,0 +1,17 @@
+from graph_tool.all import *
+import numpy as np
+
+adj = np.loadtxt('adj_300_12_5.dat')
+
+g = Graph()
+g.add_vertex(adj.shape[0])
+
+for i, vv in enumerate(adj):
+	for j, v in enumerate(vv):
+		if v == 1 and i > j:
+			g.add_edge(i, j)
+
+# g = collection.data["celegansneural"]
+state = minimize_nested_blockmodel_dl(g, deg_corr=True)
+# draw_hierarchy(state, output="celegans_nested_mdl.pdf")
+draw_hierarchy(state, output="model_nested_mdl.pdf")
