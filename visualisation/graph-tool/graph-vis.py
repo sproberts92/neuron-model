c7c389229bdd397a78c26d1765364bd81e1c2d7c
Samuel Roberts <sproberts92@outlook.com>
2015-11-28 23:41:54 +0100

    Add proper interface with argparser
    Either process a particular input file or generate the graph for
    C. elegans with the -c flag.

--- visualisation/graph-tool/graph-vis.py
+++ visualisation/graph-tool/graph-vis.py
@@ -1,21 +1,39 @@
 from graph_tool.all import *
 import numpy as np
+import argparse
+import os
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('path', nargs='?', default='.')
+    group.add_argument('-c', '--celeg', nargs='?', const=1, default=0)
+    args = parser.parse_args()
+    return args
 
 def main():
-    adj = np.loadtxt('input/adj_300_12_5.dat')
+    args = arg_parse()
 
-    g = Graph()
-    g.add_vertex(adj.shape[0])
+    if args.celeg:
+        g = collection.data["celegansneural"]
+        outpath = "output/celegans.pdf"
+    else:
+        tail = os.path.split(args.path)[-1]
+        name = os.path.splitext(tail)[0]
+        outpath = os.path.join('output','{0}.pdf'.format(name))
 
-    for i, vv in enumerate(adj):
-        for j, v in enumerate(vv):
-            if v == 1 and i > j:
-                g.add_edge(i, j)
+        adj = np.loadtxt(args.path)
 
-    # g = collection.data["celegansneural"]
+        g = Graph()
+        g.add_vertex(adj.shape[0])
+
+        for i, vv in enumerate(adj):
+            for j, v in enumerate(vv):
+                if v == 1:
+                    g.add_edge(i, j)
+
     state = minimize_nested_blockmodel_dl(g, deg_corr=True)
-    # draw_hierarchy(state, output="celegans_nested_mdl.pdf")
-    draw_hierarchy(state, output="output/model_nested_mdl.pdf")
+    draw_hierarchy(state, output=outpath)
 
 if __name__ == '__main__':
     main()
