df8032c80a46180d6fa561e06ddcc7680c485322
Samuel Roberts <sproberts92@outlook.com>
2016-02-01 16:54:31 +0100

    Set up for taking mean over statistcal runs

--- visualisation/activity/vis_activity.py
+++ visualisation/activity/vis_activity.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.loadtxt("..\\..\\output\\activity.dat")
+data = []
 
-plt.plot(data)
+for i in range(3):
+	data.append(np.loadtxt("..\\..\\output\\activity\\activity__100_10_{0}.dat".format(i)))
+
+av = [sum(e)/len(data) for e in zip(*data)]
+plt.plot(av)
 plt.show()
