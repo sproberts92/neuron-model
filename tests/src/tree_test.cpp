0cfe492454b95267a9a7de66dfee95cc10bee2eb
Samuel Roberts <sproberts92@outlook.com>
2016-02-23 11:38:32 +0100

    Add tree connection test case
    Creates one closed loop and check that the signal moves completely
    around twice.

--- tests/src/tree_test.cpp
+++ tests/src/tree_test.cpp
@@ -6,11 +6,11 @@ TEST_CASE("Tree set up", "[tree]")
 	std::valarray<std::pair<double, double>> bounds = {{-10.0, 10.0}, {-10.0, 10.0}, {-10.0, 10.0}};
 
 	Tree test_tree(bounds, all);
-	
+
 	SECTION("Tree construction")
 	{
 		std::valarray<double> test_position = test_tree.get_root()->get_pos();
-		
+
 		for (int i = 0; i < test_position.size(); ++i)
 		{
 			REQUIRE(test_position[i] > bounds[i].first);
@@ -28,7 +28,7 @@ TEST_CASE("Tree set up", "[tree]")
 
 		test_tree.grow_axon(grow_length);
 
-		Node *root_node = test_tree.get_root(); 
+		Node *root_node = test_tree.get_root();
 		std::vector<Node*> next_nodes = root_node->get_next();
 
 		for (auto i : next_nodes)
@@ -45,12 +45,12 @@ TEST_CASE("Tree set up", "[tree]")
 		for (auto i : next_nodes)
 		{
 			bool result = valarrays_are_close(
-				i->get_pos() - root_node->get_pos(), 
+				i->get_pos() - root_node->get_pos(),
 				grow_length * test_tree.get_grow_dir(),
 				0.1);
 
 			REQUIRE(result);
-		}		
+		}
 	}
 }
 
@@ -67,8 +67,24 @@ TEST_CASE("Test tree branching", "[tree]")
 	SECTION("Test branching")
 	{
 		test_target.grow_axon(grow_length);
-		auto return_val = test_tree.grow_branch(test_target, grow_length, 100.0);
+		test_tree.grow_axon(grow_length);
 
-		REQUIRE(return_val != nullptr);
+		test_tree.grow_branch(test_target, grow_length, 100.0);
+		test_target.grow_branch(test_tree, grow_length, 100.0);
+
+		test_target.get_root()->set_value(true);
+
+		int test_sum = 0;
+		for (int i = 0; i < 2 *  all.size(); ++i)
+		{
+			test_sum += test_tree.get_root()->get_value();
+
+			for (auto i : all)
+				i->push_temp_next();
+			for (auto i : all)
+				i->pop_temp(0.0);
+		}
+
+		REQUIRE(test_sum == 2);
 	}
 }
