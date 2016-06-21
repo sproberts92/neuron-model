ed71feb4dee8bbf0520061778e6ed9988030d2e9
Samuel Roberts <sproberts92@outlook.com>
2016-06-21 02:08:42 +0200

    Rework frame-processing.py
    For making nicer plots for the final presentation

--- visualisation/frame-processing/Visualisation.py
+++ visualisation/frame-processing/Visualisation.py
@@ -3,34 +3,54 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d
 from multiprocessing import Pool
 import numpy as np
+from joblib import Parallel, delayed
+from tqdm import trange
+import warnings as warn
 
-def f(i, fig, ax):
-    sigpath = "output\\signal\\signal_" + str(i) + ".dat"
+signal_path = 'output\\signal\\signal_{0}.dat'
+network_path = 'output\\Complete_Network.dat'
+neuron_path = 'output\\Complete_Network_Neurons.dat'
 
-    points = np.loadtxt(sigpath)
+def f(i, fig, ax, signals=0):
     ax.view_init(30, float(i) / float(545)* 360)
-    
-    if points.size:
-        x, y, z = points.T
-        lines = ax.scatter(x, y, z, c='r')
-        fig.canvas.draw()
 
-        plt.savefig("output\\frames\\frame" + str(i) + ".png", format='png')
-        lines.remove()
+    if signals:
+        lines = plot_scatt(signal_path.format(i), ax, c='r')
+        if lines:
+            fig.canvas.draw()
+            save_fig(i)
+            lines.remove()
+        else:
+            save_fig(i)
     else:
-        plt.savefig("output\\frames\\frame" + str(i) + ".png", format='png')
-    
-    print("Finished processing frame {0}".format(i))
+        save_fig(i)
+
+def save_fig(i):
+    plt.savefig('output\\frames\\frame' + str(i) + ".png", format='png')
+
+def load_points(path):
+    with warn.catch_warnings():
+        # To catch "Empty input file" warning
+        warn.simplefilter('ignore', category=UserWarning)
+        return np.loadtxt(path)
+
+def plot_scatt(path, ax, **kwargs):
+
+    points = load_points(path).T
+
+    if points.size:
+        x, y, z = points
+        return ax.scatter(x, y, z, **kwargs)
+    else:
+        return None
 
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection ='3d')
     ax.set_axis_off()
 
-    netpath = "output\\Complete_Network.dat"
-    points = np.loadtxt(netpath)
-    x, y, z = points.T
-    net = ax.scatter(x, y, z, c='b', marker='.', s=5, alpha=0.2)
+    plot_scatt(network_path, ax, c='b', marker='.', s=5, alpha=0.02)
+    plot_scatt(neuron_path, ax, c='b', marker='.', s=50, alpha=0.5)
 
     limits = 40
     ax.set_xlim3d(-limits,limits)
@@ -38,10 +58,8 @@ def main():
     ax.set_zlim3d(-limits,limits)
 
     ax.view_init(30,0)
-    args = [(i, fig, ax) for i in range(545)]
-    
-    p = Pool(8)
-    p.starmap(f, args, 1)
+
+    Parallel(n_jobs=4)(delayed(f)(i, fig, ax, True) for i in trange(545))
 
 if __name__ == '__main__':
     main()
