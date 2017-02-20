0ad9239de302b1419342b53ade317e6b649a6121
Samuel Roberts <sproberts92@outlook.com>
2017-02-20 20:27:44 +0100

    Add comments and clean up for Node

--- src/node.cpp
+++ src/node.cpp
@@ -25,25 +25,33 @@ void Node::push_temp_next(void)
 	for(auto &it_next : next)
 		it_next->buffer = it_next->buffer + value;
 
+	// This isn't usually needed, but is here more for safety, in case pop_temp
+	// is changed such that the value isn't overridden and instead is updated.
 	value = 0;
 }
 
 bool Node::pop_temp(double noise, Statistics &s)
 {
-	auto r_gen = rand_gen<double>(0.0, 1.0);
+	// Could this be made into a static class member?
+	static auto r_gen = rand_gen<double>(0.0, 1.0);
 
+	// Propagate if node is on and if has not been prevented by noise.
 	if(on && (noise == 0.0 || (noise != 1.0 && r_gen.get_rnum() > noise)))
 		value = buffer > 0 ? 1 : 0;
 
+	// Buffer is cleared in advance for the next propagation step.
 	buffer = 0;
 
+	// !! explicitly converts to bool.
 	return !!value;
 }
 
 void Node::update_threshold(void) {}
 
-Neuron::Neuron(const std::valarray<double> p, int t) : Node(p), thresh(t), last_visited(0)
+Neuron::Neuron(const std::valarray<double> p, int t)
+	: Node(p), thresh(t), last_visited(0)
 {
+	// To do: read the history length from config.
 	history.resize(1<<9, 0);
 }
 
@@ -51,20 +59,19 @@ void Neuron::gather_statistics(Statistics &s)
 {
 	s.pattern_found.clear();
 
+	// To do: read this from config.
 	int len = 6;
-	std::array<bool, 6> pat_win;
-
-	for (int i = 0; i < len; ++i)
-		pat_win[i] = history[i];
 
 	for (int i = len; i < history.size() - len; ++i)
 	{
 		int success = 0;
 
-		if (std::accumulate(pat_win.begin(), pat_win.end(), 0) >= 3)
+		// Look for the last len elements of the history at all points further
+		// back in the history.
+		if (std::accumulate(history.begin(), history.begin() + len, 0) >= 3)
 		{
 			for (int j = 0; j < len; ++j)
-				if (history[i + j] == pat_win[j])
+				if (history[i + j] == history[j])
 					success++;
 				else
 					break;
@@ -78,12 +85,17 @@ void Neuron::gather_statistics(Statistics &s)
 
 bool Neuron::pop_temp(double noise, Statistics &s)
 {
-	auto r_gen = rand_gen<double>(0.0, 1.0);
+	static auto r_gen = rand_gen<double>(0.0, 1.0);
 
-	if(on && buffer >= thresh && (noise == 0.0 || (noise != 1.0 && r_gen.get_rnum() > noise)))
+	// On propagation, do the usual updates but also update the history and
+	// last visited statistic (which is harder to update from inside the
+	// gather_statistics function without saving a bool for the success of
+	// propagation and passing it in.
+	if(on && buffer >= thresh && (noise == 0.0
+		|| (noise != 1.0 && r_gen.get_rnum() > noise)))
 	{
-		s.last_visited = last_visited;
 		value = buffer;
+		s.last_visited = last_visited;
 		history[0] = true;
 	}
 	else
@@ -101,21 +113,28 @@ bool Neuron::pop_temp(double noise, Statistics &s)
 
 void Neuron::update_threshold(void)
 {
+	// To do: either rename this function, or move all of this functionality
+	// into pop_temp (after checking that noting will be broken).
 	last_visited++;
 	history.pop_back();
 	history.push_front(false);
 }
 
-Synapse::Synapse(const std::valarray<double> p, int t) : Node(p), target_age(t), last_visited(-1), window(20.0f) {}
+Synapse::Synapse(const std::valarray<double> p, int t)
+	: Node(p), target_age(t), last_visited(-1), window(20.0f) {}
 
 bool Synapse::pop_temp(double noise, Statistics &s)
 {
-	auto r_gen = rand_gen<double>(0.0, 1.0);
-	if(		on
-			// && last_visited > target_age
-			&& r_gen.get_rnum() > std::max((target_age - last_visited)/window, (double)0.0f)
-			&& (noise == 0.0 || (noise != 1.0 && r_gen.get_rnum() > noise))
-		){
+	static auto r_gen = rand_gen<double>(0.0, 1.0);
+
+	// Chemotactic synapse lag is implemented here.
+	auto thr = std::max((target_age - last_visited)/window, (double)0.0f);
+
+	if(	on
+		// && last_visited > target_age
+		&& r_gen.get_rnum() > thr
+		&& (noise == 0.0 || (noise != 1.0 && r_gen.get_rnum() > noise)))
+	{
 		value = buffer;
 	}
 
--- src/node.hpp
+++ src/node.hpp
@@ -8,31 +8,48 @@
 #include <numeric>
 #include "rand_gen.hpp"
 
-typedef struct
+// POD class for containment of statistics gathered during signal propagation.
+struct Statistics
 {
 	int last_visited = 0;
 	std::vector<int> pattern_found;
-} Statistics;
+};
 
+// A basic node class from which the network will be build. Used without
+// specialisation for Schwann cells, and with specialisation for Soma and
+// Synapse nodes.
 class Node
 {
 public:
-	Node();
 	Node(const std::valarray<double> p);
 
 	bool get_value(void);
 	void set_value(const bool v);
 
+	// Turning a node off prevents propagation of a signal through the node.
 	void turn_on(void);
 	void turn_off(void);
 
 	std::valarray<double> get_pos(void);
 	std::vector<Node*> get_next(void);
+
+	// Wrapper for next.push_back(&n).
 	void push_next(Node &n);
 
+	// Clear all signals in the node. In most cases this will mean zeroing the
+	// value and buffer variables, but could mean something else if other
+	// values are added.
 	void clear_signal(void);
+
+	// Propagate signal from the value field in the current node to the buffer
+	// in each of the nodes pointed to in the `next` vector.
 	void push_temp_next(void);
+
+	// Pop the signal from the buffer into the actual value varible. Returns
+	// true if the buffer had contents that were popped, and false otherwise.
 	virtual bool pop_temp(double noise, Statistics &s);
+
+	// Update propagation thresholds where relevant (in derived classes).
 	virtual void update_threshold(void);
 
 protected:
@@ -42,9 +59,15 @@ protected:
 
 private:
 	const std::valarray<double> pos;
+
+	// Todo: replace with smart pointers.
 	std::vector<Node*> next;
 };
 
+// A specialisation of the Node class for use as a neuron body (soma) cell.
+// The primary purpose of this specialisation is to change propagation rules
+// for this type of cell and to add the ability to gather additional statistics
+// beyond the base Node class on signal throughput.
 class Neuron : public Node
 {
 public:
@@ -54,12 +77,24 @@ public:
 
 private:
 	int thresh;
+
+	// Counts the number of timesteps since a signal was last present on this
+	// neuron.
 	int last_visited;
+
+	// Stores the history of the state of the node (signal or no signal) over
+	// the last n timesteps.
 	std::deque<bool> history;
 
+	// Gathers any statistics on the current state of signals in the neuron. At
+	// the moment this is the historical pattern search but anything else
+	// needed can be added here too.
 	void gather_statistics(Statistics &s);
 };
 
+// A specialisation of the Node class for use as a synapse. The primary purpose
+// if this specialisation is to change propagation rules to add plasticity and
+// more complicated conditional firing.
 class Synapse : public Node
 {
 public:
