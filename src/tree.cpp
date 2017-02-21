060cdb65ed542d119a4e1a39ea12eb3ab6123dc9
Samuel Roberts <sproberts92@outlook.com>
2017-02-21 01:05:12 +0100

    Add comments and clean up for Tree

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,46 +1,49 @@
 #include "tree.hpp"
 
-Tree::Tree(std::valarray<std::pair<double, double>> b, int cr, std::vector<Node*> &a, std::vector<Synapse*> &s)
+Tree::Tree(std::valarray<std::pair<double, double>> b,
+	int cr, std::vector<Node*> &a, std::vector<Synapse*> &s)
 	: bounds(b), all(&a), all_s(&s)
 {
 	root = new Neuron(r_vec(bounds), cr);
 	all->push_back(root);
 
+	// Generate and normalise a vector in a random direction along which the
+	// axon will be grown.
 	grow_dir = r_vec(unit_box(bounds.size()));
 	normalise(grow_dir);
 }
 
-Neuron *Tree::get_root(void)
-{
-	return root;
-}
+Neuron *Tree::get_root(void){ return root; }
+std::valarray<double> Tree::get_grow_dir(void){ return grow_dir; }
 
-std::valarray<double> Tree::get_grow_dir(void)
-{
-	return grow_dir;
-}
-
-double Tree::find_shortest(const Tree &target, Node **shortest_ptr, std::valarray<double> &vec_r, user_config_t &cf)
+double Tree::find_shortest(const Tree &target, Node **shortest_ptr,
+	std::valarray<double> &vec_r, user_config_t &cf)
 {
 	Node *list_ptr = target.root;
 
 	double shortest_r = std::numeric_limits<double>::infinity();
 
+	// Move along the axon originating from target.root
 	while(!list_ptr->get_next().empty())
 	{
+		// Because of the periodic boundary conditions,  if a particular target
+		// node is more than box_l/2 away from the root of this tree we need to
+		// wrap around and get distance as if it was wrapped around.   This  is
+		// achieved by creating a temp node, shifting it into the adjacent cell
+		// and then measuring the distance to that instead.
 		auto temp = list_ptr->get_pos() - root->get_pos();
 
 		for (int i = 0; i < size(temp); ++i)
 		{
 			auto box_l = cf.bounds[2*i + 1] - cf.bounds[2 * i];
 
-			/* Impose periodic boundary conditions. Isn't it beautiful? */
+			// Impose periodic boundary conditions. Isn't it beautiful?
 			temp[i] -= ((temp[i] > box_l/2) - (temp[i] < -box_l/2)) * box_l;
 		}
 
 		double r = (temp * temp).sum();
 
-		/* Compare r^2 values, don't need to waste time with sqrt */
+		// Compare r^2 values, don't need to waste time with sqrt.
 		if(r < shortest_r)
 		{
 			shortest_r = r;
@@ -51,7 +54,6 @@ double Tree::find_shortest(const Tree &target, Node **shortest_ptr, std::valarra
 		list_ptr = list_ptr->get_next().front();
 	}
 
-	/* CHECK UP ON BOUNDARY CONDITIONS*/
 	return sqrt(shortest_r);
 }
 
@@ -60,14 +62,18 @@ std::valarray<double> Tree::r_vec(std::valarray<std::pair<double, double>> box)
 	std::valarray<double> vec(box.size());
 	static rand_gen<double> r_gen(0, 1);
 
+	// Generate a random number for each element of the vector then scale and
+	// translate appropriately such that if lies in the box.
 	for (int i = 0; i < vec.size(); ++i)
-		vec[i] = ((box[i].second - box[i].first) * r_gen.get_rnum() + box[i].first);
+		vec[i] = (box[i].second - box[i].first)
+			* (r_gen.get_rnum() + box[i].first);
 
 	return vec;
 }
 
 std::valarray<std::pair<double, double>> Tree::unit_box(size_t d)
 {
+	// To do: replace this entirely with a compile-time template.
 	std::valarray<std::pair<double, double>> box(d);
 	box = std::make_pair(-1, 1);
 
@@ -81,8 +87,10 @@ void Tree::normalise(std::valarray<double> &v)
 
 Node *Tree::add_node(Node *add_at, std::valarray<double> rel_pos)
 {
+	// The new position is calculated relative to the position of add_at, if
+	// it lies outside the system box then it needs to be wrapped to the other
+	// side of the box.
 	auto new_pos = rel_pos + add_at->get_pos();
-
 	impose_bc(new_pos);
 
 	Node *new_axon = new Node(new_pos);
@@ -94,8 +102,9 @@ Node *Tree::add_node(Node *add_at, std::valarray<double> rel_pos)
 
 Synapse *Tree::add_synapse(Node *add_at, std::valarray<double> rel_pos, int t)
 {
+	// To do: The functionality here is so close to Tree::add_node, it should
+	// be possible to combine them to reduce duplication.
 	auto new_pos = rel_pos + add_at->get_pos();
-
 	impose_bc(new_pos);
 
 	Synapse *new_axon = new Synapse(new_pos, t);
@@ -120,7 +129,6 @@ void Tree::impose_bc(std::valarray<double> &p)
 {
 	for (int i = 0; i < p.size(); i++)
 	{
-		// Impose periodic boundary conditions
 		if(p[i] < bounds[i].first)  p[i] += (bounds[i].second - bounds[i].first);
 		if(p[i] > bounds[i].second) p[i] -= (bounds[i].second - bounds[i].first);
 	}
@@ -128,15 +136,21 @@ void Tree::impose_bc(std::valarray<double> &p)
 
 Node *Tree::grow_branch(Tree &target, user_config_t &cf)
 {
+	// Encapsulates the process of selecting the point to grow the branch
+	// (dendrite) to, deciding whether a branch will actually be grown (with
+	// Gaussian probability) and then growing the branch.
+
 	Node *shortest = nullptr;
 
 	std::valarray<double> vec_r;
 	double r = find_shortest(target, &shortest, vec_r, cf);
 
+	// Decide whether the branch will be created, short circuits to avoid
+	// actually drawing the random variable and calculating the Gaussian if
+	// it's not necessary.
 	static rand_gen<double> r_gen = rand_gen<double>(0, 1);
-	/* TO DO: replace static with smart-pointer that can be gc'd later*/
-	/* TO DO: read constants in gaussian from config.cfg */
-	if(r != 0 && cf.link_fwhm_param != 0 && r_gen.get_rnum() < gaussian(r, cf.link_fwhm_param))
+	if(r != 0 && cf.link_fwhm_param != 0
+		&& r_gen.get_rnum() < gaussian(r, cf.link_fwhm_param))
 	{
 		vec_r = cf.schwann_l * vec_r / r;
 		Synapse *synapse = add_synapse(shortest, vec_r, cf.target_age);
--- src/tree.hpp
+++ src/tree.hpp
@@ -9,40 +9,66 @@
 #include "node.hpp"
 #include "config.h"
 
+// A tree is one neuron/axon/dendrite(s) system. The root node is the neuron,
+// from which the axon is grown. Dendrites are then grown from neighbouring
+// axons back the the root neuron.
 class Tree
 {
 public:
-	Tree();
-	Tree(std::valarray<std::pair<double, double>> b, int cr, std::vector<Node*> &a, std::vector<Synapse*> &s);
+	Tree(std::valarray<std::pair<double, double>> b, int cr,
+		std::vector<Node*> &a, std::vector<Synapse*> &s);
 
 	Neuron *get_root(void);
 	std::valarray<double> get_grow_dir(void);
 
+	// Grow one unit of axon with length l in the direction grow_dir.
 	void grow_axon(double l);
+
+	// Grow a branch (dendrite) from another axon (that which emerges from
+	// target) the root neuron of this tree.
 	Node *grow_branch(Tree &target, user_config_t &cf);
 
 private:
+	// Create a unit box.
 	std::valarray<std::pair<double, double>> unit_box(size_t d);
-	std::valarray<double> r_vec(std::valarray<std::pair<double, double>> box);
-	void normalise(std::valarray<double> &v);
 
-	double find_shortest(const Tree &target, Node **shortest_ptr, std::valarray<double> &vec_r, user_config_t &cf);
-	Node *add_node(Node *add_at, std::valarray<double> g_dir);
-	Synapse *add_synapse(Node *add_at, std::valarray<double> g_dir, int t);
+	// Generate a random vector that lies within box.
+	std::valarray<double> r_vec(std::valarray<std::pair<double, double>> box);
+
+	// Find the point on a target axon at which the distance to the root node
+	// of this tree is the shortest.
+	double find_shortest(const Tree &target, Node **shortest_ptr,
+		std::valarray<double> &vec_r, user_config_t &cf);
+
+	// Add a new node positioned relative to the node add_at.
+	Node *add_node(Node *add_at, std::valarray<double> rel_pos);
+
+	// Add a new synapse node positioned relative to the node add_at.
+	Synapse *add_synapse(Node *add_at, std::valarray<double> rel_pos, int t);
+
+	// Translate a point p such that it lies within the system bounds.
 	void impose_bc(std::valarray<double> &p);
+
+	void normalise(std::valarray<double> &v);
 	double gaussian(double x, double c);
 
 	Neuron *root;
+
+	// Pointers to the arrays which keep track of the whole network/all of the
+	// synapses.
+	// To do: replace with smart pointers.
 	std::vector<Node*> *all;
 	std::vector<Synapse*> *all_s;
+
 	std::valarray<double> grow_dir;
 	std::valarray<std::pair<double, double>> bounds;
 };
 
+// A comparison for use in the depth first path search in the Brain class.
 struct TreeRootComp
 {
  	explicit TreeRootComp(Node *n) : node(n) { }
- 	inline const bool operator()(Tree & m) const { return m.get_root() == node; }
+ 	inline bool operator()(Tree & m) const { return m.get_root() == node; }
 private:
  	Node *node;
 };
